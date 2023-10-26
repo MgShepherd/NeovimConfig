36e27b9defd928beffe4b856293dba929efa28c5
MgShepherd <michaelshep1504@gmail.com>
2023-10-26 19:30:51 +0100

    Fixed conflicting keymaps

--- lua/michael/plugins/nvim-lint.lua
+++ lua/michael/plugins/nvim-lint.lua
@@ -1,29 +1,27 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
-    
-    lint.linters_by_ft = {
-	    javascript = { "eslint_d" },
-	    typescript = { "eslint_d" },
-	    javascriptreact = { "eslint_d" },
-	    typescriptreact = { "eslint_d" },
-    }
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+		}
 
-    vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
-	    group = lint_augroup,
-	    callback = function()
-		    require("lint").try_lint()
-	    end,
-    })
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.keymap.set("n", "<leader>l", function()
-	    lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end
+		vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>l", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting for current file" })
+	end,
 }
-
-
